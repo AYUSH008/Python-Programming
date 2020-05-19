16d3ba221ced2d99e6d1d8a14c8cb24f353c4e23
Ayush <asatkar.ayush@gmail.com>
2020-05-19 15:20:36 +0530

    input and variable

---
+++ Input and variable.py
@@ -0,0 +1,20 @@
+# single line comment
+""" How to take input in python and reuseable the variables"""
+
+print("welcome to the code")
+a = input("Take a input to demostrate the input in python\n")
+"""Escape sequence"""
+print("Number is ", a)
+
+# multi line comment
+""" this is 
+multi line 
+comment"""
+
+# variable reuseability
+a = 3
+print(a)
+a = 8.5
+print(a)
+a = "Hello world"
+print(a)
--- main.py
+++
@@ -1,9 +0,0 @@
-import flask
-print("Hello world 6")
-a= input("Number")
-print("number is ", a) 
-
-#multi line comment
-""" this is 
-multi line 
-comment"""
